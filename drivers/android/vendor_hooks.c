c1c035ead7e1db5b9576d90a8cb29494dcb2e07f
Pan, Kris <kris.pan@intel.com>
2025-06-23 12:16:47 +0000

    Revert "ANDROID: vendor_hooks: add hooks for f2fs_printk"
    This reverts commit 3444d6a933ecbe9ccb5e24250f6a292ebbfc81d9.

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -152,7 +152,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_transfer_rsp_status);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_f2fs_down_read);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_f2fs_improve_priority);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_f2fs_restore_priority);
-EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_f2fs_printk);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_io_statistics);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_dpm_prepare);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ogki_check_vip_status);
