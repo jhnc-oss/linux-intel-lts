e237fc05dfb0ae278aea69fb897ff72af0a9e3cf
Pan, Kris <kris.pan@intel.com>
2025-06-23 12:16:40 +0000

    Revert "ANDROID: writeback: add vendor hooks to do a hierarchical writeback control"
    This reverts commit 28b35bdaea49d8d7737889bd9068908c2a241108.

--- fs/inode.c
+++ fs/inode.c
@@ -26,7 +26,6 @@
 
 #undef CREATE_TRACE_POINTS
 #include <trace/hooks/vmscan.h>
-#include <trace/hooks/fs.h>
 
 /*
  * Inode locking rules:
@@ -711,7 +710,6 @@ static void evict(struct inode *inode)
 	if (!list_empty(&inode->i_io_list))
 		inode_io_list_del(inode);
 
-	trace_android_vh_evict(inode);
 	inode_sb_list_del(inode);
 
 	spin_lock(&inode->i_lock);
