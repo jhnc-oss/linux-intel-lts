3b552c6b8ccb4a254c16c27b28c82493291ba04d
Pan, Kris <kris.pan@intel.com>
2025-07-31 03:20:18 +0000

    driver: media: i2c: fix ar0234 compile error
    Signed-off-by: Pan, Kris <kris.pan@intel.com>

--- drivers/media/i2c/ar0234.c
+++ drivers/media/i2c/ar0234.c
@@ -503,7 +503,7 @@ static int ar0234_set_ctrl(struct v4l2_ctrl *ctrl)
 		break;
 
 	case V4L2_CID_HFLIP:
-	case V4L2_CID_VFLIP:
+	case V4L2_CID_VFLIP: {
 		u64 reg;
 
 		ret = cci_read(ar0234->regmap, AR0234_REG_ORIENTATION,
@@ -521,7 +521,7 @@ static int ar0234_set_ctrl(struct v4l2_ctrl *ctrl)
 		ret = cci_write(ar0234->regmap, AR0234_REG_ORIENTATION,
 				reg, NULL);
 		break;
-
+	}
 	case V4L2_CID_TEST_PATTERN:
 		ret = cci_write(ar0234->regmap, AR0234_REG_TEST_PATTERN,
 				ar0234_test_pattern_val[ctrl->val], NULL);
