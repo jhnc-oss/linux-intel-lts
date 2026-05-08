9a785f6235b239eecb2ece3c36c75f1a03f92df6
Qiuxu Zhuo <qiuxu.zhuo@intel.com>
2026-05-08 15:31:12 +0800

    EDAC/igen6: Add Intel Starfire SoCs support
    Starfire is a derivative of Panther Lake SoC and shares a similar memory
    subsystem architecture. Add Starfire compute die ID and reuse Panther
    Lake's configuration data for EDAC support.
    
    Signed-off-by: Qiuxu Zhuo <qiuxu.zhuo@intel.com>

--- drivers/edac/igen6_edac.c
+++ drivers/edac/igen6_edac.c
@@ -314,6 +314,9 @@ static struct work_struct ecclog_work;
 #define DID_PTL_H_SKU13	0xb02a
 #define DID_PTL_H_SKU14	0xb00a
 
+/* Compute die IDs for Starfire with IBECC */
+#define DID_STF_SKU1	0xb02b
+
 /* Compute die IDs for Wildcat Lake with IBECC */
 #define DID_WCL_SKU1	0xfd00
 
@@ -864,6 +867,7 @@ static struct pci_device_id igen6_pci_tbl[] = {
 	{ PCI_VDEVICE(INTEL, DID_PTL_H_SKU12), (kernel_ulong_t)&ptl_h_cfg },
 	{ PCI_VDEVICE(INTEL, DID_PTL_H_SKU13), (kernel_ulong_t)&ptl_h_cfg },
 	{ PCI_VDEVICE(INTEL, DID_PTL_H_SKU14), (kernel_ulong_t)&ptl_h_cfg },
+	{ PCI_VDEVICE(INTEL, DID_STF_SKU1), (kernel_ulong_t)&ptl_h_cfg },
 	{ PCI_VDEVICE(INTEL, DID_WCL_SKU1), (kernel_ulong_t)&wcl_cfg },
 	{ },
 };
